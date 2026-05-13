58579743bdac532c946a647fad88450d3db0ec47
kacperjanicki <kacperekjanicki9@gmail.com>
2026-05-13 16:27:23 +0200

    init

---
+++ Controllers/PcsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_cw7_EntityFramework.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PcsController : ControllerBase
+{
+    
+    [HttpGet]
+    public IActionResult GetPCs()
+    {
+        return Ok();
+    }
+}
